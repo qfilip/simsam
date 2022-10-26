32934a2eceff68ed8a6ff146e1090852c50da78d
phq <kukolj.filip@gmail.com>
2022-10-27 01:20:27 +0200

    grpc samples

---
+++ Grpc_Sol/Grpc.Server/Models/CocktailModel.cs
@@ -0,0 +1,12 @@
+﻿using Google.Protobuf.Collections;
+
+namespace Grpc.Server.Models;
+
+class CocktailModel
+{
+    public string? Name { get; set; }
+    public IEnumerable<string>? Ingredients { get; set; }
+
+    public static CocktailDto Map(CocktailModel model) =>
+        new CocktailDto()
+}
---
+++ Grpc_Sol/Grpc.Server/Services/CocktailService.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+using Grpc.Server.Models;
+using Microsoft.Extensions.ObjectPool;
+
+namespace Grpc.Server.Services;
+
+public class CocktailService : Server.Cocktail.CocktailBase
+{
+    public override async Task<CocktailDto> GetAll(GetByNameRequest request, ServerCallContext context)
+    {
+        var models = GetModels();
+        request.
+
+    }
+
+    private IEnumerable<CocktailModel> GetModels()
+    {
+        Func<string, string[], CocktailModel> create = (name, ings) =>
+            new CocktailModel { Name = name, Ingredients = ings };
+
+        return new List<CocktailModel>
+        {
+            create("Negroni", new string[] {"Gin", "Vermuth Roso", "Campari" }),
+            create("Dark n Stormy", new string[] {"Rum", "Ginger Ale", "Mint" }),
+            create("Basil Bramble", new string[] {"Gin", "sweet & sour mix", "Basil leaves", "Creme de Mure" })
+        };
+    }
+}
+
